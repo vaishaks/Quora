3d882a4cf15cf5a7ed57f131b29c26edca29664e
vaishak <vaishak93@gmail.com>
2013-07-22 01:46:44 +0530

    Read input from STDIN instead of file

---
+++ submit.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from json import loads
+from json import dumps
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction import DictVectorizer
+
+N = int(raw_input())
+training_data_list = []
+test_data_list = []
+
+for i in xrange(N):
+    json_string = raw_input()
+    training_data_list.append(loads(json_string))
+
+T = int(raw_input())
+for i in xrange(T):
+    json_string = raw_input()
+    test_data_list.append(loads(json_string))
+    
+features = []
+label = []
+features_new = []
+for train_ele in training_data_list:
+    features.append({'sum':sum([x['followers'] for x in \
+                                    train_ele['topics']])})
+    label.append({'__ans__':1 if train_ele['__ans__'] == True else 0})
+
+for test_ele in test_data_list:
+    features_new.append({'sum':sum(x['followers'] for x in \
+                                       test_ele['topics'])})
+
+vec = DictVectorizer()
+X = vec.fit_transform(features).toarray()
+y = vec.fit_transform(label).toarray()
+X_new = vec.fit_transform(features_new).toarray()
+
+clf = LinearSVC()
+clf = clf.fit(X, y)
+y_new = clf.predict(X_new)
+
+out = []
+for test_ele, l in zip(test_data_list, y_new):
+    out.append({'__ans__':str(l==1), 'question_key':test_ele['question_key']})
+
+for o in out:
+    print dumps(o)
