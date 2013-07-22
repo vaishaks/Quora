b4151cd9daaea179c2ca40fbdbba94a90d481d35
vaishak <vaishak93@gmail.com>
2013-07-22 19:43:30 +0530

    Added cross-validation with test data

--- train.py
+++ train.py
@@ -5,9 +5,11 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_extraction import DictVectorizer
 
 f = open('Data/answered_data_10k.in')
+f2 = open('Data/answered_data_10k.out')
 N = int(f.readline())
 training_data_list = []
 test_data_list = []
+test_out_list = []
 
 for i in xrange(N):
     json_string = f.readline()
@@ -17,31 +19,34 @@ T = int(f.readline())
 for i in xrange(T):
     json_string = f.readline()
     test_data_list.append(loads(json_string))
-    
+
+for i in xrange(T):
+    json_string = f2.readline()
+    test_out_list.append(loads(json_string))
+
+f.close()
+f2.close()
 features = []
 label = []
 features_new = []
+label_new = []
 for train_ele in training_data_list:
     features.append({'sum':sum([x['followers'] for x in \
                                     train_ele['topics']])})
     label.append({'__ans__':1 if train_ele['__ans__'] == True else 0})
 
-for test_ele in test_data_list:
+for test_ele, test_out in zip(test_data_list, test_out_list):
     features_new.append({'sum':sum(x['followers'] for x in \
                                        test_ele['topics'])})
+    label_new.append({'__ans__':1 if test_out['__ans__'] == \
+                          True else 0})
 
 vec = DictVectorizer()
 X = vec.fit_transform(features).toarray()
 y = vec.fit_transform(label).toarray()
 X_new = vec.fit_transform(features_new).toarray()
+y_new = vec.fit_transform(label_new).toarray()
 
 clf = LinearSVC()
 clf = clf.fit(X, y)
-y_new = clf.predict(X_new)
-
-out = []
-for test_ele, l in zip(test_data_list, y_new):
-    out.append({'__ans__':str(l==1), 'question_key':test_ele['question_key']})
-
-for o in out:
-    print dumps(o)
+print clf.score(X_new, y_new)
